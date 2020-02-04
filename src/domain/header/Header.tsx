18cf8a390a9ea43cb8106e78f6e6a057df55e0ef
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-02-04 16:59:21 +0100

    Render the tabs in the header
    Use the Tabs component of the materialUI library to render
    the tabs and apply CSS styles to separate each tab
    following the outline given in "template.png".

--- src/domain/header/Header.tsx
+++ src/domain/header/Header.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 
-import { makeStyles, AppBar } from '@material-ui/core'
+import { makeStyles, AppBar, Tabs, Tab } from '@material-ui/core'
 
 import { themeValues } from 'app/themes/themeValues'
 
@@ -11,14 +11,47 @@ const useStyles = makeStyles({
 		position: 'relative',
 		color: 'white',
 	},
+	tabs: {
+		bottom: 0,
+		position: 'absolute',
+	},
+	tab1: {
+		backgroundColor: 'white',
+		margin: '20px 10px 0px 20px',
+	},
+	tab: {
+		margin: '20px 10px 0px 10px',
+		backgroundColor: 'white',
+	},
 })
 
 const Header: FC = () => {
 	const classes = useStyles()
 
+	const [value, setValue] = React.useState(0)
+
+	const handleChange: any = (
+		event: React.ChangeEvent<{}>,
+		newValue: number
+	) => {
+		setValue(newValue)
+	}
+
 	return (
 		<div className={classes.root}>
 			<AppBar position="static" />
+			<Tabs
+				className={classes.tabs}
+				value={value}
+				onChange={handleChange}
+				indicatorColor="primary"
+				textColor="primary"
+				centered
+			>
+				<Tab className={classes.tab1} label="Tab 1" />
+				<Tab className={classes.tab} label="Tab 2" />
+				<Tab className={classes.tab} label="Tab 3" />
+			</Tabs>
 		</div>
 	)
 }
