88cf1e95d91485348fc0a1374e622e5fadb662a6
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-02-06 12:47:47 +0100

    Use shareData component
    In the children implement the shareData component and
    access to the properties through boolean, also deleted
    any fetch since now it is delegated to the shareData
    parent component.

--- src/domain/drawer/Drawer.tsx
+++ src/domain/drawer/Drawer.tsx
@@ -1,12 +1,26 @@
 import React, { FC } from 'react'
-import MuiDrawer, {
-	DrawerProps as MuiDrawerProps,
-} from '@material-ui/core/Drawer'
+import { DrawerProps as MuiDrawerProps } from '@material-ui/core/Drawer'
+
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Chip from '@material-ui/core/Chip'
+
+import ShareData from '../../component/ShareData'
 
 export type DrawerProps = MuiDrawerProps
 
-const Drawer: FC<DrawerProps> = props => {
-	return <MuiDrawer variant="permanent" {...props} />
+const Drawer: FC<DrawerProps> = () => {
+	return (
+		<List>
+			{['Title'].map(text => (
+				<ListItem button key={text}>
+					<ListItemText primary={<ShareData name />} />
+					<Chip size="small" label={<ShareData status />} />
+				</ListItem>
+			))}
+		</List>
+	)
 }
 
 export default Drawer
--- src/domain/welcome/ProjectPageID.tsx
+++ src/domain/welcome/ProjectPageID.tsx
@@ -2,20 +2,7 @@
 import React, { FC } from 'react'
 import { makeStyles, Theme, Paper } from '@material-ui/core'
 
-import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
-import { ApolloProvider } from '@apollo/react-common'
-
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-
-import StatusButton from '../../component/StatusButton'
-
-const client = new ApolloClient({
-	link: new HttpLink({ uri: 'http://localhost:9002/graphql' }),
-	cache: new InMemoryCache(),
-})
+import ShareData from '../../component/ShareData'
 
 const useStyles = makeStyles((theme: Theme) => ({
 	root: {
@@ -26,137 +13,17 @@ const useStyles = makeStyles((theme: Theme) => ({
 const ProjectPageID: FC = ({ children }) => {
 	const classes = useStyles()
 
-	const TABS = gql`
-		query Tabs {
-			allCompanies {
-				name
-				industry
-				employees {
-					firstName
-					lastName
-					address
-				}
-			}
-		}
-	`
-
-	const STATUS = gql`
-		query Tabs {
-			allCompanies {
-				status
-			}
-		}
-	`
-
-	const Company = (): any => {
-		const { data, error, loading } = useQuery(TABS)
-		if (loading) {
-			return <div>Loading...</div>
-		}
-		if (error) {
-			return <div>Error! {error.message}</div>
-		}
-
-		return (
-			<div>
-				{data &&
-					data.allCompanies.map((company: any, index: number) => (
-						<div key={index}>
-							{index === 0 ? (
-								<div>
-									<h1>{company.name}</h1>
-									<h2>{company.industry}</h2>
-									<h3>Employees</h3>
-									{company.employees.map(
-										(
-											employee: any,
-											indexEmployee: number
-										) => (
-											<div key={indexEmployee}>
-												<table>
-													<tbody>
-														<tr>
-															<td>
-																{' '}
-																{
-																	employee.firstName
-																}{' '}
-																{' | '}{' '}
-															</td>
-															<td>
-																{' | '}{' '}
-																{
-																	employee.lastName
-																}
-																{' | '}{' '}
-															</td>
-															<td>
-																{' | '}{' '}
-																{
-																	employee.address
-																}{' '}
-															</td>
-														</tr>
-													</tbody>
-												</table>
-											</div>
-										)
-									)}
-								</div>
-							) : (
-								''
-							)}
-						</div>
-					))}
-			</div>
-		)
-	}
-
-	const Status = (): any => {
-		const { data, error, loading, refetch } = useQuery(STATUS)
-		if (loading) {
-			return <div>Loading...</div>
-		}
-		if (error) {
-			return <div>Error! {error.message}</div>
-		}
-
-		return (
-			<div>
-				{data &&
-					data.allCompanies.map((company: any, index: number) => (
-						<div key={index}>
-							{index === 0 ? (
-								<div>
-									<h3>Status</h3>
-									<h4>{company.status}</h4>
-									<StatusButton
-										text="+"
-										disabled={false}
-										onClick={() => refetch()}
-									/>
-									<StatusButton
-										text="-"
-										disabled={false}
-										onClick={() => refetch()}
-									/>
-								</div>
-							) : (
-								''
-							)}
-						</div>
-					))}
-			</div>
-		)
-	}
-
 	return (
 		<div className={classes.root}>
 			<Paper className={classes.root}>
-				<ApolloProvider client={client}>
-					<Company />
-					<Status />
-				</ApolloProvider>
+				<ShareData
+					name
+					industry
+					titleEmployees
+					employees
+					status
+					buttons
+				/>
 			</Paper>
 			{children}
 		</div>
